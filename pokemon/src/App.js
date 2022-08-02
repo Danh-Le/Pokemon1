f070ab7e07050c49bece8e0f3414131c24f91dd3
Danh-Le <quang.danh.le@gmail.com>
2022-08-02 11:18:10 +0200

    pokemon id done

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -13,22 +13,31 @@ const App = () => {
   const fetchData = async () => {
     const request = await fetch("https://pokeapi.co/api/v2/pokemon/1");
     const response = await request.json();
-    console.log(response);
+    setPokemon(response);
   };
 
   const handleRandomClick = () => {
     setPokemon(pokemon);
   };
-
-  return (
-    <div className="">
-      {/* <h1>{name}</h1> */}
-      <p></p>
-      <p></p>
-      <p></p>
-      <button onClick={handleRandomClick}>Random</button>
-    </div>
-  );
+  if (pokemon === null) {
+    <div></div>;
+  } else
+    return (
+      <div className="">
+        <img src={pokemon.sprites.front_default} />
+        <h1>{pokemon.name}</h1>
+        <p>Height: {pokemon.height}</p>
+        <p>Weight: {pokemon.weight}</p>
+        {pokemon.types.map((type) => {
+          return (
+            <>
+              <p>Types: {type.type.name}</p>
+            </>
+          );
+        })}
+        <button onClick={handleRandomClick}>Random</button>
+      </div>
+    );
 };
 
 export default App;
