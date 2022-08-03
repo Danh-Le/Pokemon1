b3a714481f35e7c27b3a686fc201a65b44b1e079
Danh-Le <quang.danh.le@gmail.com>
2022-08-03 09:24:55 +0200

    with few style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [pokemon, setPokemon] = useState(null);
@@ -31,20 +32,22 @@ const App = () => {
     <div></div>;
   } else
     return (
-      <div className="">
-        <img src={pokemon.sprites.front_default} alt="pokemonPic" />
-        <h1>{pokemon.name}</h1>
-        <p>Height: {pokemon.height}</p>
-        <p>Weight: {pokemon.weight}</p>
-        {pokemon.types.map((type) => {
-          return (
-            <>
-              <p>Types: {type.type.name}</p>
-            </>
-          );
-        })}
-        <button onClick={handleRandomClick}>Random pokemon</button>
-      </div>
+      <section className="poke">
+        <div className="card">
+          <img src={pokemon.sprites.front_default} alt="pokemonPic" />
+          <h1>{pokemon.name}</h1>
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+          {pokemon.types.map((type) => {
+            return (
+              <>
+                <p>Types: {type.type.name}</p>
+              </>
+            );
+          })}
+          <button onClick={handleRandomClick}>Random pokemon</button>
+        </div>
+      </section>
     );
 };
 
